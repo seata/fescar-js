ec637e9b60374a9348ac50bc503cf29d70434d9a
hufeng <fengmi99@gmail.com>
2022-08-20 01:05:38 +0800

    feat: :sparkles: add byte buffer

--- packages/seata-js/src/seata-rpc-client/v1/byte-buffer.ts
+++ packages/seata-js/src/seata-rpc-client/v1/byte-buffer.ts
@@ -17,28 +17,180 @@
 
 import { Buffer } from 'node:buffer'
 
+export interface ByteBufferProp {
+  defaultAllocSize?: number
+}
+
 const DEFAULT_ALLOC_SIZE = 1024
 
-class ByteBuffer {
-  private offset: number
-  private size: number
+export default class ByteBuffer {
   private buff: Buffer
 
-  constructor() {
+  private offset: number
+  private capacity: number
+  private defaultAllocSize: number
+
+  /**
+   * constructor
+   * @param prop
+   */
+  constructor(prop?: ByteBufferProp) {
+    prop ||= { defaultAllocSize: DEFAULT_ALLOC_SIZE }
+
     this.offset = 0
-    this.size = DEFAULT_ALLOC_SIZE
-    this.buff = Buffer.alloc(this.size)
+    this.defaultAllocSize = prop.defaultAllocSize!
+    this.capacity = this.defaultAllocSize
+    this.buff = Buffer.alloc(this.capacity)
   }
 
-
-
-  writeByte(v: number, offset?: number ) {
-    if (offset === undefined) {
-
+  /**
+   * check capacity and auto expand capacity
+   *
+   * @param nextOffset
+   * @returns
+   */
+  private checkCapacity(nextOffset: number) {
+    if (nextOffset < this.capacity) {
+      return
     }
+
+    // expand capacity
+    this.capacity += Math.max(nextOffset - this.capacity, this.defaultAllocSize)
+    const buff = Buffer.alloc(this.capacity)
+    this.buff.copy(buff, 0, 0, this.offset)
+    this.buff = buff
   }
 
-  get capacity() {
-    return this.size - this.offset
+  /**
+   * add one byte to buffer
+   *
+   * @param val
+   * @param offset
+   * @returns
+   */
+  addByte(val: number, offset?: number) {
+    offset ||= this.offset
+    const next = offset + 1
+
+    this.checkCapacity(next)
+    this.buff.writeUInt8(val, offset)
+
+    if (next > this.offset) {
+      this.offset = next
+    }
+
+    return this
+  }
+
+  /**
+   * add two bytes to buffer
+   *
+   * @param val number
+   * @param offset offset
+   * @param endian 'big' | 'little'
+   * @returns
+   */
+  addShort(val: number, offset?: number, endian: 'big' | 'little' = 'big') {
+    offset ||= this.offset
+    const next = offset + 2
+    this.checkCapacity(next)
+
+    const isSign = val >= -32768 && val <= 32767
+    if (endian === 'big') {
+      isSign
+        ? this.buff.writeInt16BE(val, offset)
+        : this.buff.writeUInt16BE(val, offset)
+    } else {
+      isSign
+        ? this.buff.writeInt16LE(val, offset)
+        : this.buff.writeUint16LE(val, offset)
+    }
+
+    if (next > this.offset) {
+      this.offset = next
+    }
+
+    return this
+  }
+
+  /**
+   * add four bytes to buffer
+   *
+   * @param val number
+   * @param offset offset
+   * @param endian 'big' | 'little'
+   * @returns
+   */
+  addInt(val: number, offset?: number, endian: 'big' | 'little' = 'big') {
+    offset ||= this.offset
+    const next = offset + 4
+    this.checkCapacity(next)
+
+    const isSign = val >= -2147483648 && val <= 2147483647
+    if (endian === 'big') {
+      isSign
+        ? this.buff.writeInt32BE(val, next)
+        : this.buff.writeUInt32BE(val, next)
+    } else {
+      isSign
+        ? this.buff.writeInt32LE(val, next)
+        : this.buff.writeUint32LE(val, next)
+    }
+
+    if (next > this.offset) {
+      this.offset = next
+    }
+
+    return this
+  }
+
+  /**
+   * add bytes to buffer
+   * @param val
+   * @param offset
+   * @returns
+   */
+  addBytes(val: Buffer, offset?: number) {
+    offset ||= this.offset
+    const next = offset + val.length
+
+    this.checkCapacity(next)
+    val.copy(this.buff, offset, 0)
+
+    if (next > this.offset) {
+      this.offset = next
+    }
+
+    return this
+  }
+
+  /**
+   * add string to buffer
+   * @param val
+   * @param offset
+   * @returns
+   */
+  addString(val: string, offset?: number) {
+    const buf = Buffer.from(val)
+
+    offset ||= this.offset
+    const next = offset + buf.length
+    this.checkCapacity(next)
+    buf.copy(this.buff, offset, 0)
+
+    if (next > this.offset) {
+      this.offset = next
+    }
+
+    return this
+  }
+
+  /**
+   * get buffer
+   *
+   * @returns buffer
+   */
+  buffer() {
+    return this.buff.subarray(0, this.offset)
   }
 }
